2f59adcb470611da351047ea7548f8474546119b
KaramSEA <karamserroukhamazon@gmail.com>
2024-11-15 13:41:42 +0100

    commit

---
+++ KSD/JavaScriptEntCliente/practicClass.js
@@ -0,0 +1,21 @@
+function encontrarMaximo(arr) {
+    let max =arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        
+        
+    }
+}
+
+function calcularPromedio(arr) {
+    let num=0;
+    for (let i = 0; i < arr.length; i++) {
+        num+=arr[i];
+        
+    }
+    return num/arr.length;
+}
+function inverirArray(arr) {
+    
+}
+
+
